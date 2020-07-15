55685ca05e1c3317d66abf47b3a3d40e456de19b
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-14 21:51:52 -0400

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "50px";
 
 // move dodger to left
-
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
@@ -14,35 +13,25 @@ function moveDodgerLeft() {
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
-  }
+}
+
+// move dodger to right
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+}
 
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
-  });
-
-// move dodger to right
-function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
-    let right = parseInt(rightNumbers, 10);
-   
-    // if (right < 0) {
-      dodger.style.right = `${right + 1}px`;
-    // }
-}
+});
 
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
       moveDodgerRight();
     }
 });
-
-// document.addEventListener("keydown", function(e) {
-//     if (e.key === "ArrowRight") {
-//       let rightNumbers = dodger.style.right.replace("px", "");
-//       let right = parseInt(rightNumbers, 10);
-   
-//       dodger.style.right = `${right - 1}px`;
-//     }
-//   });
