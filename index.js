2047e044af1c9df3fccb5afa38a5b0c2d6f50f9d
N-Santiago <norbertosantiago82@gmail.com>
2020-09-23 14:43:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let dodger = document.getElementById("dodger");
+const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px"; 
 
+const leftNumbers = dodger.style.left.replace("px", "");
+
 function moveDodgerLeft() {
-    let leftNumbers = dodger.style.left.replace("px", "");
+    leftNumbers
     let left = parseInt(leftNumbers, 10);
-   
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
@@ -18,9 +19,8 @@ function moveDodgerLeft() {
   });
 
 function moveDodgerRight() {
-    let leftNumbers = dodger.style.left.replace("px", "");
+    leftNumbers
     let left = parseInt(leftNumbers, 10);
-   
     if (left > 0) {
       dodger.style.left = `${left + 1}px`;
     }
